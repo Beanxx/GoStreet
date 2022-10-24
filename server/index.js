727f10b1eca37f30225b7d83e37a6c08d4f8e593
Beanxx <tnqls991211@naver.com>
2022-10-25 00:30:38 +0900

    feat: MongoDB 연결

--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const path = require("path"); // server 파일 경로와 client 파일 경로 합쳐주는 모듈
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 4000;
+const config = require("./config/key.js");
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  mongoose
+    .connect(config.mongoURI)
+    .then(() => {
+      console.log(`Example app listening on port ${port}`);
+      console.log("connecting MongoDB...");
+    })
+    .catch((err) => {
+      console.log(`${err}`);
+    });
 });
 
 app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
