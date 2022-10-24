a4e73323098e40d6a3e8313cd7283de2641e8e22
Beanxx <tnqls991211@naver.com>
2022-10-25 01:28:42 +0900

    feat: Mongoose Model 적용

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const { Post } = require("./Model/Post.js");
+
 app.listen(port, () => {
   mongoose
     .connect(config.mongoURI)
@@ -33,6 +35,8 @@ app.get("*", (req, res) => {
 });
 
 app.post("/api/test", (req, res) => {
-  console.log(req.body);
-  res.status(200).json({ success: true, text: "안녕하세요" });
+  const CommunityPost = new Post({ title: "test", content: "testing" });
+  CommunityPost.save().then(() => {
+    res.status(200).json({ success: true, text: "안녕하세요" });
+  });
 });
