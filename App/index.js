e6b7f1c9a3e247d4c924ea6c4f9e060070060969
Beanxx <tnqls991211@naver.com>
2022-10-30 22:06:57 +0900

    feat: heroku 배포 설정 & 폴더 구조 변경

--- App/index.js
+++ App/index.js
@@ -3,19 +3,19 @@ const path = require("path"); // server 파일 경로와 client 파일 경로 �
 const mongoose = require("mongoose");
 
 const app = express();
-const port = 4000;
-const config = require("./config/key.js");
+const port = process.env.PORT || 4000;
+const config = require("./server/config/key.js");
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // client에서 보내는 body 명령어 추적 가능해짐!
 app.use(express.json());
 app.use("/image", express.static("./image")); // 현재 경로에 있는 이미지 폴더 안에 있는 것들을 이미지로 사용하겠다!
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/post", require("./Router/post.js"));
-app.use("/api/user", require("./Router/user.js"));
-app.use("/api/reple", require("./Router/reple.js"));
+app.use("/api/post", require("./server/Router/post.js"));
+app.use("/api/user", require("./server/Router/user.js"));
+app.use("/api/reple", require("./server/Router/reple.js"));
 
 app.listen(port, () => {
   mongoose
@@ -30,9 +30,9 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
