238f1cb690d7048c1a7b328a53937e5e7ced190c
Beanxx <tnqls991211@naver.com>
2022-11-02 18:44:11 +0900

    feat: 게시글 최신순, 인기순 정렬 기능

--- App/client/src/components/MainPage.js
+++ App/client/src/components/MainPage.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import List from "./post/List.js";
 import axios from "axios";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 
 const MainPage = () => {
   const [postList, setPostList] = useState([]);
+  const [sort, setSort] = useState("최신순");
 
   useEffect(() => {
+    let body = {
+      sort,
+    };
     axios
-      .post("/api/post/list")
+      .post("/api/post/list", body)
       .then((response) => {
         if (response.data.success) {
           setPostList([...response.data.postList]);
@@ -16,10 +21,14 @@ const MainPage = () => {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [sort]);
 
   return (
     <div>
+      <DropdownButton variant="outline-secondary" title={sort}>
+        <Dropdown.Item onClick={() => setSort("최신순")}>최신순</Dropdown.Item>
+        <Dropdown.Item onClick={() => setSort("인기순")}>인기순</Dropdown.Item>
+      </DropdownButton>
       <div>
         <input type="text" />
         <button></button>
